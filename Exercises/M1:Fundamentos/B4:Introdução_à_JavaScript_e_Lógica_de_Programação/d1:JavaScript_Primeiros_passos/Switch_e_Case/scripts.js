efc389e733040092f65aac80872cfd2bfa39f0fc
1lucascq <1lucascq@gmail.com>
2021-07-28 12:42:22 -0300

    Adiciona exercicios de fixaçao do conteudo

---
+++ Exercises/M1:Fundamentos/B4:Introdução_à_JavaScript_e_Lógica_de_Programação/d1:JavaScript_Primeiros_passos/Switch_e_Case/scripts.js
@@ -0,0 +1,25 @@
+//Armazene o status de uma pessoa, que pode ser: Aprovada; Reprovada ou Lista:
+let status = "lasd";
+switch (status) {
+  case "aprovada":
+    console.log("Aprovada =D")
+    break
+  }
+  
+switch (status) {
+  case "reprovada":
+    console.log("Reprovada")
+    break
+  }
+  
+switch (status) {
+  case "lista":
+    console.log("Lista de espera")
+    break
+  }
+
+switch (status) {
+  default:
+    console.log("Não se aplica")
+    break
+}
---
+++ Exercises/M1:Fundamentos/B4:Introdução_à_JavaScript_e_Lógica_de_Programação/d1:JavaScript_Primeiros_passos/Variáveis/scripts.js
@@ -0,0 +1,6 @@
+const myName = "Lucas";
+const birthCity = "Montes Claros - MG";
+let birthYear = "2030";
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);

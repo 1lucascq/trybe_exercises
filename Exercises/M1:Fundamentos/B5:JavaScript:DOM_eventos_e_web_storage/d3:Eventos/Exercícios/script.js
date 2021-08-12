f1774513028ff047b89d85a6bbfed83ab693ff44
1lucascq <1lucascq@gmail.com>
2021-08-11 23:01:19 -0300

    Exercicio 2

--- Exercises/M1:Fundamentos/B5:JavaScript:DOM_eventos_e_web_storage/d3:Eventos/Exercícios/script.js
+++ Exercises/M1:Fundamentos/B5:JavaScript:DOM_eventos_e_web_storage/d3:Eventos/Exercícios/script.js
@@ -13,7 +13,12 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-// Escreva seu código abaixo.
+//          Exercício 1:
+// O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days" . Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Domingo e Segunda-feira.
+  // Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . Ex: <li class="day">3</li>
+  // Os dias 24, 25 e 31 são feriados e, além da classe day , devem conter também a classe holiday . Ex: <li class="day holiday">24</li>
+  // Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . Ex: <li class="day friday">4</li>
+
 
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
@@ -30,3 +35,28 @@ for (let day of dezDaysList) {
   }
   dezDays.appendChild(li);
 }
+
+
+//          Exercício 2:
+// Implemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
+  // Adicione a este botão a ID "btn-holiday".
+  // Adicione este botão como filho/filha da tag <div> com classe "buttons-container".
+
+function criarBotão(feriados) {
+  feriados = document.createElement('button');
+  feriados.id = 'btn-holiday';
+  feriados.innerText = 'Feriados';
+  let divButtons = document.querySelector('.buttons-container');
+  divButtons.appendChild(feriados);
+}
+criarBotão()
+
+//          Exercício 3:
+
+//          Exercício 4:
+//          Exercício 5:
+//          Exercício 6:
+//          Exercício 7:
+//          Exercício 8:
+//          Exercício 9:
+//          Exercício 10:

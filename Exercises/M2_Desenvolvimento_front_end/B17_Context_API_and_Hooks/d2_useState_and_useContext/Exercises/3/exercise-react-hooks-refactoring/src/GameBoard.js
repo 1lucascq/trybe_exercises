05ce86b46507bb1746370e84ac7f780583fb4b6a
1lucascq <1lucascq@gmail.com>
2021-12-03 17:11:17 -0300

    Exercicio 3

--- Exercises/M2_Desenvolvimento_front_end/B17_Context_API_and_Hooks/d2_useState_and_useContext/Exercises/3/exercise-react-hooks-refactoring/src/GameBoard.js
+++ Exercises/M2_Desenvolvimento_front_end/B17_Context_API_and_Hooks/d2_useState_and_useContext/Exercises/3/exercise-react-hooks-refactoring/src/GameBoard.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import GameCell from './GameCell';
+import PropTypes from 'prop-types';
 import './GameBoard.css';
 
-class GameBoard extends React.Component {
-  render() {
-    const { gameState, updateGame } = this.props;
-    return (
-      <div className="game-board">
-        {gameState.map((playerId, i) => (
-          <GameCell
-            id={i}
-            key={i}
-            onClick={() => updateGame(i)}
-            content={playerId}
-          />
-        ))}
-      </div>
-    );
-  }
+function GameBoard({ gameState, updateGame }) {
+  return (
+    <div className="game-board">
+      {gameState.map((playerId, i) => (
+        <GameCell id={i} key={i} onClick={() => updateGame(i)} content={playerId} />
+      ))}
+    </div>
+  );
 }
 
 GameBoard.propTypes = {
--- Exercises/M2_Desenvolvimento_front_end/B17_Context_API_and_Hooks/d2_useState_and_useContext/Exercises/3/exercise-react-hooks-refactoring/src/TicTacToe.js
+++ Exercises/M2_Desenvolvimento_front_end/B17_Context_API_and_Hooks/d2_useState_and_useContext/Exercises/3/exercise-react-hooks-refactoring/src/TicTacToe.js
@@ -1,129 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GameBoard from './GameBoard';
 
-class TicTacToe extends React.Component {
-  static victoryArchivedInLine(gameBoard) {
+function TicTacToe() {
+  const [activePlayer, setActivePlayer] = useState(1);
+  const [gameBoard, setGameBoard] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+  const toggleActivePlayer = () => setActivePlayer(activePlayer === 1 ? 2 : 1);
+
+  const updateState = (cellClicked) => {
+    if (gameBoard[cellClicked] !== 0) return false;
+
+    let newGameBoard = [...gameBoard];
+    newGameBoard[cellClicked] = activePlayer;
+    setGameBoard(newGameBoard);
+    toggleActivePlayer();
+  };
+
+  const victoryArchivedInLine = () => {
     for (let i = 0; i <= 6; i += 3) {
       if (
-        gameBoard[i] === gameBoard[i + 1]
-        && gameBoard[i + 1] === gameBoard[i + 2]
-        && gameBoard[i] !== 0
-      ) return gameBoard[i];
+        gameBoard[i] === gameBoard[i + 1] &&
+        gameBoard[i + 1] === gameBoard[i + 2] &&
+        gameBoard[i] !== 0
+      )
+        return true;
     }
     return false;
-  }
+  };
 
-  static victoryArchivedInColumn(gameBoard) {
-    for (let i = 0; i <= 2; i += 1) {
+  const victoryArchivedInColumn = () => {
+    for (let i = 0; i <= 2; i++) {
       if (
-        gameBoard[i] === gameBoard[i + 3]
-        && gameBoard[i + 3] === gameBoard[i + 6]
-        && gameBoard[i] !== 0
-      ) return gameBoard[i];
+        gameBoard[i] === gameBoard[i + 3] &&
+        gameBoard[i + 3] === gameBoard[i + 6] &&
+        gameBoard[i] !== 0
+      )
+        return true;
     }
     return false;
-  }
+  };
 
-  static victoryArchivedInDiagonals(gameBoard) {
+  const victoryArchivedInDiagonals = () => {
     if (gameBoard[4] === 0) return false;
-    if (gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8]) {
-      return gameBoard[0];
-    }
-    if (gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6]) {
-      return gameBoard[2];
-    }
+    if (gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8]) return true;
+    if (gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6]) return true;
     return false;
-  }
+  };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      activePlayer: 1,
-      gameBoard: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    };
-
-    this.updateState = this.updateState.bind(this);
-    this.resetGame = this.resetGame.bind(this);
-    this.renderButton = this.renderButton.bind(this);
-  }
-
-  resetGame() {
-    this.setState({
-      activePlayer: 1,
-      gameBoard: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    });
-  }
-
-  toggleActivePlayer() {
-    const { activePlayer } = this.state;
-    if (activePlayer === 1) return 2;
-    return 1;
-  }
-
-  updateState(cellClicked) {
-    this.setState((state) => {
-      const newState = [...state.gameBoard];
-      let newActivePlayer = state.activePlayer;
-
-      if (state.gameBoard[cellClicked] === 0) {
-        newState[cellClicked] = state.activePlayer;
-        newActivePlayer = this.toggleActivePlayer();
-      } else newState[cellClicked] = state.gameBoard[cellClicked];
-
-      return {
-        activePlayer: newActivePlayer,
-        gameBoard: newState,
-      };
-    });
-  }
-
-  victoryArchieved() {
-    const { gameBoard } = this.state;
-
+  const victoryArchieved = () => {
     return (
-      TicTacToe.victoryArchivedInLine(gameBoard)
-      || TicTacToe.victoryArchivedInColumn(gameBoard)
-      || TicTacToe.victoryArchivedInDiagonals(gameBoard)
+      victoryArchivedInLine() || victoryArchivedInColumn() || victoryArchivedInDiagonals()
     );
-  }
+  };
 
-  renderButton() {
-    return (
-      <button
-        type="button"
-        onClick={this.resetGame}
-        data-testid="restart-button"
-      >
-        Recomeçar Jogo
-      </button>
-    );
-  }
-
-  render() {
-    const { gameBoard } = this.state;
-    const win = this.victoryArchieved();
-    if (!gameBoard.includes(0) && !win) {
-      return (
-        <>
-          {this.renderButton()}
-          <h1>Empate</h1>
-        </>
-      );
-    }
-    return (
-      <>
-        {this.renderButton()}
-        {(!win)
-          ? (
-            <GameBoard
-              gameState={gameBoard}
-              updateGame={this.updateState}
-            />
-          )
-          : <h1>{`Player ${win === 2 ? 'O' : 'X'} Ganhou`}</h1>}
-      </>
-    );
-  }
+  if (victoryArchieved()) return <h1>Fim de Jogo</h1>;
+  return <GameBoard gameState={gameBoard} updateGame={updateState} />;
 }
 
 export default TicTacToe;

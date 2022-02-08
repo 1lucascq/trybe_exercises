6a633f45aeff6fd73f73a6987f9bbcc5daedc5e6
1lucascq <1lucascq@gmail.com>
2022-02-08 20:24:05 -0300

    Exercicios Bonus 1

--- Exercises/M3_BackEnd/B22_Introducing_NodeJS/d1_The_NodeJS_Engine/Exercises/factorial.js
+++ Exercises/M3_BackEnd/B22_Introducing_NodeJS/d1_The_NodeJS_Engine/Exercises/factorial.js
@@ -1,19 +1,21 @@
 function getFactorial(n) {
-
-      
   // --------> peguei do Course.
+
   // Se n for 0 ou 1
   return [0, 1].includes(n)
   // Retornamos 1
   ? 1
   // Caso contrário, continuamos o cálculo do fatorial multiplicando n pelo fatorial de n - 1
   : n * getFactorial(n - 1); // Uma função que chama a si mesma é chamada de função *RECURSIVA*
-  
-  console.log(`x: ${x}`);
-  
-  const resultado = getFactorial(x);
-  
-  console.log(`Resultado: ${resultado}`);
 }
 
-console.log(getFactorial(5))
+const readline = require('readline-sync');
+const number = readline.questionInt('Type a number to get their fatorial: ')
+
+if (number <= 0) {
+  console.log('0 is not a valid number!')
+} else {
+  console.log(`x: ${number}`);  
+  console.log(`Resultado: ${getFactorial(number)}`);
+}
+

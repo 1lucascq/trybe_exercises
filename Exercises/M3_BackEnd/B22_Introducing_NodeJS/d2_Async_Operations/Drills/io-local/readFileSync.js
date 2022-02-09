afdc81292958736ee77f3b4239b658083e8b3725
1lucascq <1lucascq@gmail.com>
2022-02-09 14:18:09 -0300

    promisse.All, readFile and writeFile

--- Exercises/M3_BackEnd/B22_Introducing_NodeJS/d2_Async_Operations/Drills/io-local/readFileSync.js
+++ Exercises/M3_BackEnd/B22_Introducing_NodeJS/d2_Async_Operations/Drills/io-local/readFileSync.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const nomeDoArquivo = 'meu-arquivo.txt';
 
 try {
-  const data = fs.readFileSync(nomeDoArquivo, 'binary');
+  const data = fs.readFileSync(nomeDoArquivo, 'utf8');
   console.log(data);
 } catch (err) {
   console.error(`Erro ao ler o arquivo: ${err.path}`);

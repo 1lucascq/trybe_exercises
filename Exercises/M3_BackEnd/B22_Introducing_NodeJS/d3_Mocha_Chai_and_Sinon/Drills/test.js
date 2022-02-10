89c3a3a8cba90ac7f34294c3226dd8f4885bc038
1lucascq <1lucascq@gmail.com>
2022-02-10 17:52:30 -0300

    Conteudo de fixacao completo

--- Exercises/M3_BackEnd/B22_Introducing_NodeJS/d3_Mocha_Chai_and_Sinon/Drills/test.js
+++ Exercises/M3_BackEnd/B22_Introducing_NodeJS/d3_Mocha_Chai_and_Sinon/Drills/test.js
@@ -1,12 +1,24 @@
+const fs = require('fs');
+const sinon = require('sinon');
 const { expect } = require('chai');
 const readFiles = require('./readFiles');
+
 const FILE_CONTENT =  'testing 1, 2, 3';
+
 
 describe('Read files', () => {
   describe('When the file exists', () => {
+  
+    before(() => {
+      sinon.stub(fs, 'readFileSync').returns(FILE_CONTENT);
+    });
+
+    after(() => {
+      fs.readFileSync.restore()
+    });
+
     describe('returns', () => {
       const answer = readFiles('file.txt');
-
       it('is a string', () => {
         expect(answer).to.be.a('string');
       });
@@ -18,6 +30,13 @@ describe('Read files', () => {
   });
 
   describe('When the file doesn\'t exists:', () => {
+  
+    before(() => {
+      sinon.stub(fs, 'readFileSync').throws(null);
+    })
+    
+    after(() => fs.readFileSync.restore())
+    
     describe('the return', () => {
       const answer = readFiles('fileThatDontExist.txt');
       it('is equal to "null"', () => {

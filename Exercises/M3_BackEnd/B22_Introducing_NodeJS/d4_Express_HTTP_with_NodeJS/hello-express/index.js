f93e5b69764d1c853d982b3f512a63ad2a3089bb
1lucascq <1lucascq@gmail.com>
2022-02-11 13:58:09 -0300

    Exercicios sobre roteamento

--- Exercises/M3_BackEnd/B22_Introducing_NodeJS/d4_Express_HTTP_with_NodeJS/hello-express/index.js
+++ Exercises/M3_BackEnd/B22_Introducing_NodeJS/d4_Express_HTTP_with_NodeJS/hello-express/index.js
@@ -8,6 +8,6 @@ app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
 }); // 3
 
-function handleHelloWorldRequest(req, res) {
-  res.status(200).send('Hello World!'); // 4
+function handleHelloWorldRequest(_req, res) {
+  res.status(200).send('<h1>Hello World!</h1><b><h3>Testando 1, 2, 3</h3>'); // 4
 }

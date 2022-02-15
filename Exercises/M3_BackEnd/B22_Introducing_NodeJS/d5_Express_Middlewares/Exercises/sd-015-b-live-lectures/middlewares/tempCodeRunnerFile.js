73f9109d2d2b5cebe23a0e031b403533dc3519ad
1lucascq <1lucascq@gmail.com>
2022-02-14 22:42:12 -0300

    Adiciona arquivos da dinamica em sala

--- Exercises/M3_BackEnd/B22_Introducing_NodeJS/d5_Express_Middlewares/Exercises/sd-015-b-live-lectures/middlewares/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-async function bla () {
-const fs = require('fs/promises');
-  const data = await fs.readFile('./simpsons.json', 'utf-8')
-  const simpsons = JSON.parse(data);
-  await console.log(simpsons)
-}
-bla();

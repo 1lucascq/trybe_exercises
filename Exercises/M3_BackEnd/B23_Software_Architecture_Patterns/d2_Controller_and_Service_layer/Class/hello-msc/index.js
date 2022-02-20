025b36b4f7079a3d667d341de88a8550196b744f
1lucascq <1lucascq@gmail.com>
2022-02-20 19:08:21 -0300

    Atividades de fixacao completos

---
+++ Exercises/M3_BackEnd/B23_Software_Architecture_Patterns/d2_Controller_and_Service_layer/Class/hello-msc/index.js
@@ -0,0 +1,23 @@
+// hello-msc/index.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const rescue = require('express-rescue');
+const Author = require('./controllers/Author')
+const errorMiddleware = require('./middlewares/error');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.get('/authors', rescue(Author.getAll));
+app.get('/authors/:id', rescue(Author.findById));
+app.post('/authors', rescue(Author.createAuthor));
+
+
+app.use(errorMiddleware);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Ouvindo a porta ${PORT}`);
+});

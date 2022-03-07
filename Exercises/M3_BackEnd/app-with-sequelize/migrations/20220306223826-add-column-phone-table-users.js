cf3a3d0772b65cf0dcb84331713a30ff98e864a0
1lucascq <1lucascq@gmail.com>
2022-03-06 21:15:23 -0300

    Model

--- Exercises/M3_BackEnd/app-with-sequelize/migrations/20220306223826-add-column-phone-table-users.js
+++ Exercises/M3_BackEnd/app-with-sequelize/migrations/20220306223826-add-column-phone-table-users.js
@@ -1,13 +1,13 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('Users', 'phone_num', {
-      type: Sequelize.STRING,
-    });
+  up: async (queryInterface, Sequelize) => {
+   await queryInterface.addColumn('Users', 'phone_num', {
+     type: Sequelize.STRING,
+   });
   },
 
-  async down (queryInterface, Sequelize) {
+  down: async (queryInterface, Sequelize) => {
     await queryInterface.removeColumn('Users', 'phone_num');
   }
 };

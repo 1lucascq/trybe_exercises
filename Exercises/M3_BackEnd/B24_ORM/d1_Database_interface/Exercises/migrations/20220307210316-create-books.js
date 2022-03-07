0e10ef8775e1e92b32a527762ce53c02a1c59fc2
1lucascq <1lucascq@gmail.com>
2022-03-07 18:24:09 -0300

    cria migration que define a tabela Books

--- Exercises/M3_BackEnd/B24_ORM/d1_Database_interface/Exercises/migrations/20220307210316-create-books.js
+++ Exercises/M3_BackEnd/B24_ORM/d1_Database_interface/Exercises/migrations/20220307210316-create-books.js
@@ -2,20 +2,33 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('Books', { 
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      author: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      pageQuantity: {
+        type: Sequelize.INTEGER,
+        field: 'page_quantity',
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        field: 'created_at',
+      },
+    })
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('Books');
   }
 };

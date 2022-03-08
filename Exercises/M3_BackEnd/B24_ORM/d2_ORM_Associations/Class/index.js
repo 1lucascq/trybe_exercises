eb16f1120f3f4923845071f5d2788fabab0c2ce2
1lucascq <1lucascq@gmail.com>
2022-03-08 18:06:54 -0300

    Adiciona rota de testes do Relacionamento N:N

--- Exercises/M3_BackEnd/B24_ORM/d2_ORM_Associations/Class/index.js
+++ Exercises/M3_BackEnd/B24_ORM/d2_ORM_Associations/Class/index.js
@@ -49,6 +49,24 @@ app.get('/employees/:id', async (req, res) => {
   };
 });
 
+app.get('/usersbooks/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findOne({
+      where: { userId: id },
+      include: [{ model: Book, as: 'books', through: { attributes: [] } }],
+    });
+
+    if (!user)
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+
+    return res.status(200).json(user);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`On: ${PORT}`));
 

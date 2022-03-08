488cd62f0a720733dea71a5219e97ba8be11ea5f
1lucascq <1lucascq@gmail.com>
2022-03-08 13:41:33 -0300

    Finaliza exercicios bonus com o test dos arquivos

--- Exercises/M3_BackEnd/B24_ORM/d1_Database_interface/Exercises/src/models/Book.js
+++ Exercises/M3_BackEnd/B24_ORM/d1_Database_interface/Exercises/src/models/Book.js
@@ -3,9 +3,10 @@ module.exports = (sequelize, DataTypes) => {
     title: DataTypes.STRING,
     author: DataTypes.STRING,
     pageQuantity: DataTypes.INTEGER,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
   },
   {
-    updatedAt: false,
     underscored: true,
   })
 
---
+++ Exercises/M3_BackEnd/B24_ORM/d1_Database_interface/Exercises/tests/unit/models/Model.test.js
@@ -0,0 +1,27 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+
+const Book = require("../../../src/models/Book
+");
+
+const {
+  sequelize,
+  dataTypes,
+  checkModelName,
+  checkPropertyExists,
+} = require('sequelize-test-helpers');
+
+describe('O model de Book', () => {
+  const Book = BookModel(sequelize, dataTypes);
+  const book = new Book();
+
+  describe('possui o nome "Book"', () => {
+    checkModelName(Book)('Book');
+  });
+
+  describe('possui as propriedades "fullName" e "email"', () => {
+    ['title', 'author', 'pageQuantity'].forEach(checkPropertyExists(book));
+  });
+});

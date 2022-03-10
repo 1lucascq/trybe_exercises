adb6cc1a134ff621132129faa9638812c99022a0
1lucascq <1lucascq@gmail.com>
2022-03-10 18:33:54 -0300

    24-4: Testes de integraçao metodo findAll

---
+++ Exercises/M3_BackEnd/B24_ORM/d4_Integration_tests/Class/nodejs-jwt-base-project/tests/createUsers.test.js
@@ -0,0 +1,42 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const chaiHttp = require('chai-http')
+const server = require('../api/app');
+
+// Importação do modelo original, contido em `models`, a partir da raiz
+const { User } = require('../models');
+// Importação do mock utilizado nesse contexto
+const { User: userMock }  = require('./mock/models')
+
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Rota /api/users', () => {
+  before(() => {
+    sinon.stub(User, 'findAll').callsFake(userMock.findAll);
+  })
+  after(() => {
+    User.findAll.restore();
+  })
+    describe('Consulta a lista de pessoas usuárias', () => {
+        let response;
+
+        before(async () => {
+            response = await chai.request(server).get('/api/  ');
+        });
+
+        it(
+            'A requisição GET para a rota traz uma lista inicial ' +
+            'contendo dois registros de pessoas usuárias',
+            () => {
+              expect(response.body).to.have.length(2);
+            }
+        );
+
+        it('Essa requisição deve retornar código de status 200', () => {
+            expect(response).to.have.status(200);
+        });
+    });
+});

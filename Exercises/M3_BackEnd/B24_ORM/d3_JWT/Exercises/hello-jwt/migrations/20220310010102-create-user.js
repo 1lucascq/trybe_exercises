9282ddc9a90f25608a3a252d19be65c7199ca24f
1lucascq <1lucascq@gmail.com>
2022-03-09 22:08:37 -0300

    cria schema e migration para exercicio

---
+++ Exercises/M3_BackEnd/B24_ORM/d3_JWT/Exercises/hello-jwt/migrations/20220310010102-create-user.js
@@ -0,0 +1,32 @@
+/* eslint-disable max-lines-per-function */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      login: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('Users');
+  },
+};

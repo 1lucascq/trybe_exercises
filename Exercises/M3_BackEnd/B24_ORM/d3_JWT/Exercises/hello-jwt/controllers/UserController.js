1e7c5cfbfd05910887df3c22d78acfeed6837a72
1lucascq <1lucascq@gmail.com>
2022-03-10 16:06:34 -0300

    Adiciona variaveis de conexao do JWT

--- Exercises/M3_BackEnd/B24_ORM/d3_JWT/Exercises/hello-jwt/controllers/UserController.js
+++ Exercises/M3_BackEnd/B24_ORM/d3_JWT/Exercises/hello-jwt/controllers/UserController.js
@@ -1,10 +1,19 @@
+const jwt = require('jsonwebtoken');
 const { User } = require('../models');
+
+const SECRET = 'batata';
+const JWT_CONFIG = {
+  expiresIn: '7d',
+  algorithm: 'HS256',
+};
 
 const create = async (req, res) => {
   try {
     const { login, password } = req.body;
     const userData = await User.create({ login, password });
-    return res.status(200).json({ id: userData.id, login, password });
+    const TOKEN = jwt.sign({ userData }, SECRET, JWT_CONFIG);
+    console.log(TOKEN);
+    return res.status(200).json({ TOKEN });
   } catch (e) {
     console.log(e.message);
     return res.status(500).json({ message: 'Deu ruim' });

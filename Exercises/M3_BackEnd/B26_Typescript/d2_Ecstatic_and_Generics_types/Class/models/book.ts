935b3497a796a8c32b623047e318b5510cf6017b
1lucascq <1lucascq@gmail.com>
2022-03-24 17:17:06 -0300

    feat: adiciona sessao de manejo de db com typescript

---
+++ Exercises/M3_BackEnd/B26_Typescript/d2_Ecstatic_and_Generics_types/Class/models/book.ts
@@ -0,0 +1,23 @@
+import { Pool, ResultSetHeader } from "mysql2/promise";
+
+export interface Book{
+  id?: number
+  title: string
+  price: number
+  author: string
+  isbn: string
+}
+
+export default class BookModel {
+  private connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async getAll(): Promise<Book[]> {
+    const result = await this.connection.execute('SELECT * FROM books');
+    const [rows] = result;
+    return rows as Book[];
+  }
+}

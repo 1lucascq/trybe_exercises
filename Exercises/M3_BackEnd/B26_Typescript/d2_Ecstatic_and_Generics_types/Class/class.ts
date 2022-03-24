ab06d6ebfbadc38f4e971ae0da2d1bfee572c408
1lucascq <1lucascq@gmail.com>
2022-03-24 13:31:24 -0300

    feat: exercicios de fixacao - classes

---
+++ Exercises/M3_BackEnd/B26_Typescript/d2_Ecstatic_and_Generics_types/Class/class.ts
@@ -0,0 +1,73 @@
+enum EyeColor {
+  Black = "Pretos",
+  Blue = "Azuis",
+  Green = "Verdes",
+  Brown = "Castanhos",
+}
+
+// usamos a palavra reservada class para definir uma classe
+class Person {
+  name: string;
+  birthDate: Date; // o tipo Date está presente no TypeScript assim como no JavaScript
+  eyeColor?: EyeColor; // na cor dos olhos usamos uma Enum com valores pré definidos ou undefined = ?
+
+  // aprenderemos mais sobre o construtor no próximo bloco
+  // considere-o como uma função utilizada para construir um objeto a partir da classe
+  // nele recebemos todos os dados necessários para construir um objeto de pessoa
+  constructor(name: string, birthDate: Date, eyeColor?: EyeColor) { // union type entre o EyeColor e undefined 
+    
+      // usamos o this para acessar as propriedades da instância da classe
+      // ele representa a própria instância que estamos criando
+      // atribuímos o valor do parâmetro recebido à propriedade da instância da classe
+      this.name  = name;
+      this.birthDate  = birthDate;
+      this.eyeColor  = eyeColor;
+  }
+
+  speak(): void {
+      console.log(`${this.name} está falando.`);
+  }
+
+  eat(): void {
+      console.log(`${this.name} está comendo.`)
+  }
+
+  walk(): void {
+      console.log(`${this.name} está andando.`)
+  }
+}
+
+// usamos a palavra reservada new para criar uma instância de Person
+// e passamos os parâmetros necessários para o construtor
+const person1 = new Person("Jane Doe", new Date("1986-01-01"));
+const person2 = new Person("Jon Doe", new Date("1980-08-05"), EyeColor.Black);
+
+console.log(person1);
+person1.speak()
+
+// saída:
+// Person: {
+//   "name": "Jane Doe",
+//   "birthDate": "1986-01-01T00:00:00.000Z",
+// }
+// "Jane Doe está falando."
+person1.eyeColor = EyeColor.Blue
+console.log(person1);
+// saída:
+// Person: {
+//   "name": "Jane Doe",
+//   "birthDate": "1986-01-01T00:00:00.000Z",
+//   "eyeColor": "Azuis"
+// }
+
+
+console.log(person2);
+person2.walk();
+
+// saída:
+// Person: {
+//   "name": "Jon Doe",
+//   "birthDate": "1980-08-05T00:00:00.000Z",
+//   "eyeColor": "Pretos"
+// }
+// "Jon Doe está andando."
--- Exercises/M3_BackEnd/B26_Typescript/d2_Ecstatic_and_Generics_types/Class/fixation.ts
+++ Exercises/M3_BackEnd/B26_Typescript/d2_Ecstatic_and_Generics_types/Class/fixation.ts
@@ -8,73 +8,45 @@ type Sum = (
   y: number,
 ) => number;
 
-type MaterType = 'solid' | 'liquid' | 'gas';
+type MaterType = 'solid' | 'liquid' | 'gaseous';
 
 type OS = 'linux' | 'windows';
 
 type CPF = '123.567.890-12' | '123456789012';
 
-enum EyeColor {
-  Black = "Pretos",
-  Blue = "Azuis",
-  Green = "Verdes",
-  Brown = "Castanhos",
+
+enum Size {
+  small = 's',
+  medium = 'm',
+  big = 'b'
 }
-
-// usamos a palavra reservada class para definir uma classe
-class Person {
-  name: string;
-  birthDate: Date; // o tipo Date está presente no TypeScript assim como no JavaScript
-  eyeColor: EyeColor; // na cor dos olhos usamos uma Enum com valores pré definidos
-
-  // aprenderemos mais sobre o construtor no próximo bloco
-  // considere-o como uma função utilizada para construir um objeto a partir da classe
-  // nele recebemos todos os dados necessários para construir um objeto de pessoa
-  constructor(name: string, birthDate: Date, eyeColor: EyeColor) {
-      // usamos o this para acessar as propriedades da instância da classe
-      // ele representa a própria instância que estamos criando
-      // atribuímos o valor do parâmetro recebido à propriedade da instância da classe
-      this.name  = name;
-      this.birthDate  = birthDate;
-      this.eyeColor  = eyeColor;
+interface Dog {
+  name: string,
+  size: Size,
+  color: string
+}
+class Dog {
+  constructor (name: string, size: Size, color: string) {
+    this.name = name;
+    this.size = size;
+    this.color = color;
   }
 
-  speak(): void {
-      console.log(`${this.name} está falando.`);
-  }
-
-  eat(): void {
-      console.log(`${this.name} está comendo.`)
-  }
-
-  walk(): void {
-      console.log(`${this.name} está andando.`)
+  bark(): string {
+    if (this.size === 's') return `${this.name} says: auau in ${this.color}`
+    if (this.size === 'm') return `${this.name} says: AUAU in ${this.color}`
+    if (this.size === 'b') return `${this.name} says: AUAUAUAUAUAUAUAU in ${this.color}`
+    return 'This dog doesn\'t bark'
   }
 }
+const pudou = new Dog('Pudo', Size.small, 'white')
+const pluto = new Dog('Pluto', Size.medium, 'yellow')
+const brazilianViraLata = new Dog('Caramelo', Size.big, 'caramelês')
 
-// usamos a palavra reservada new para criar uma instância de Person
-// e passamos os parâmetros necessários para o construtor
-const person1 = new Person("Jane Doe", new Date("1986-01-01"), EyeColor.Brown);
-const person2 = new Person("Jon Doe", new Date("1980-08-05"), EyeColor.Black);
+console.log(pudou.bark())
+console.log(brazilianViraLata.bark())
+console.log(pluto.bark())
 
-console.log(person1);
-person1.speak()
 
-// saída:
-// Person: {
-//   "name": "Jane Doe",
-//   "birthDate": "1986-01-01T00:00:00.000Z",
-//   "eyeColor": "Castanhos"
-// }
-// "Jane Doe está falando."
 
-console.log(person2);
-person2.walk();
 
-// saída:
-// Person: {
-//   "name": "Jon Doe",
-//   "birthDate": "1980-08-05T00:00:00.000Z",
-//   "eyeColor": "Pretos"
-// }
-// "Jon Doe está andando."

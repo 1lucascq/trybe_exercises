ce0c99c9cdc8fbf19a73e5d835e460b98e8e1fe3
1lucascq <1lucascq@gmail.com>
2022-03-24 15:05:32 -0300

    feat: exercicio 1

---
+++ Exercises/M3_BackEnd/B26_Typescript/d2_Ecstatic_and_Generics_types/Exercises/ex/car.ts
@@ -0,0 +1,57 @@
+enum Color {
+  black = 'preto',
+  white = 'branco',
+}
+
+enum Direction {
+  right = 'direita',
+  left = 'esquerda',
+}
+
+interface Car {
+  brand: string,
+  color: Color,
+  doors: number,
+}
+class Car {
+  constructor (brand: string, color: Color, doors: number) {
+    this.brand = brand;
+    this.color = color;
+    this.doors = doors;
+  }
+
+  honk(): string {
+    return 'BEEP BEEP'
+  }
+
+  manageDoors(isOpen: boolean): string {
+    isOpen = !isOpen
+    if (isOpen) return 'portas abertas';
+    return 'portas fechadas';
+  }
+
+  turnOnAndOff(on: boolean): string {
+    on = !on
+    if (on) return 'carro ligado';
+    return 'carro desligado';
+  }
+
+  speedingUp(speedUp: boolean): string {
+    if (speedUp) return 'tacale pau marcos';
+    return 'desacelerando';
+  }
+
+  doBrake(): string {
+    return 'PERAE PARO'
+  }
+
+  turn(direction: Direction): string {
+    if (direction === 'direita') return 'virando pra direita'
+    return 'virando pra esquerda'
+  }
+}
+
+const FuscaoPreto = new Car('VW', Color.black, 4)
+console.log(FuscaoPreto)
+console.log(FuscaoPreto.speedingUp(true))
+console.log(FuscaoPreto.honk())

4d24ea69b14eba005625e4e8b5429438b3ad096e
1lucascq <1lucascq@gmail.com>
2022-03-24 14:12:00 -0300

    feat: exercicios de fixacao - classes

--- Exercises/M3_BackEnd/B26_Typescript/d2_Ecstatic_and_Generics_types/Class/class.ts
+++ Exercises/M3_BackEnd/B26_Typescript/d2_Ecstatic_and_Generics_types/Class/class.ts
@@ -109,3 +109,83 @@ let teacher: Teacher = {
       return `Olá, eu sou ${this.fullName()} e leciono ${this.subject}`;
   }
 }
+
+// -------------------------------------------------------- GENERICS
+// COM O ANY --> errado
+// function getArray(items : any[]) : any[] {
+//   return new Array().concat(items);
+// }
+
+// let numberArray = getArray([5, 10, 15, 20]);
+// let stringArray = getArray(["Cats", "Dogs", "Birds"]);
+// numberArray.push(25);
+// stringArray.push("Rabbits");
+// numberArray.push("isto não é um número");
+// stringArray.push(30);
+// console.log(numberArray);
+// // Saída:  [5, 10, 15, 20, 25, "isto não é um número"]
+// console.log(stringArray);
+// // Saída: ["Cats", "Dogs", "Birds", "Rabbits", 30]
+
+
+// T pode ser coke coisa
+function getArray<T>(items : T[]) : T[] {
+  return new Array<T>().concat(items);
+}
+
+let numberArray = getArray<number>([5, 10, 15, 20]);
+numberArray.push(25);
+// numberArray.push("isto não é um número"); com any havia erro, com o generic (no caso <T>) não há
+console.log(numberArray);
+// Saída:  [5, 10, 15, 20, 25, "isto não é um número"]
+
+let stringArray = getArray<string>(["Cats", "Dogs", "Birds"]);
+stringArray.push("Rabbits");
+// stringArray.push(30); com any havia erro, com o generic (no caso <T>) não há
+console.log(stringArray);
+// Saída: ["Cats", "Dogs", "Birds", "Rabbits", 30]
+
+function identity<T, U> (value: T, message: U) : T {
+  console.log(message);
+  return value
+}
+
+let returnNumber = identity<number, string>(100, "Olá");
+let returnString = identity<string, string>("100", "Mundo");
+let returnBoolean = identity<boolean, string>(true, "Olá, Mundo!");
+
+/*
+                 --> GENERICS com INTERFACES
+interface ProcessIdentity<T, U> {
+  (value: T, message: U): T;
+}
+
+function processIdentity<T, U> (value: T, message: U) : T {
+  console.log(message);
+  return value
+}
+
+let processor: ProcessIdentity<number, string> = processIdentity;
+let returnNum = processor(100, "Olá");
+let returnStr = processor("Olá", 100); // Type check error: Argument of type "string" is not assignable to parameter of type "number".
+*/
+
+//                  --> GENERICS em CLASSES
+interface ProcessIdentity<T, U> {
+  _value: T;
+  _message: U;
+
+}
+class ProcessIdentity<T, U> {
+  constructor(value: T, message: U) {
+      this._value = value;
+      this._message = message;
+  }
+  getIdentity() : T {
+      console.log(this._message);
+      return this._value
+  }
+}
+
+let processor = new ProcessIdentity<number, string>(100, "Olá");
+processor.getIdentity();  // imprime "Olá" e retorna 100

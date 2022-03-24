f85a7d8f3af93423381328dc4d5b466c14275da7
1lucascq <1lucascq@gmail.com>
2022-03-24 18:18:52 -0300

    feat: exercicio 4

--- Exercises/M3_BackEnd/B26_Typescript/d2_Ecstatic_and_Generics_types/Exercises/ex/pizza.ts
+++ Exercises/M3_BackEnd/B26_Typescript/d2_Ecstatic_and_Generics_types/Exercises/ex/pizza.ts
@@ -1,17 +1,23 @@
 type Slices = 4 | 6 | 8;
+type Comum = 'calabresa' | 'frango' | 'pepperoni'
+type Doce =  'nutella' | 'goiabada com queijo' | 'marshmallow' 
+type Vegetariana = 'margherita' | 'palmito' | 'cogumelo'
 
 interface Pizza {
-  flavor: string
+  flavor: Comum | Doce | Vegetariana,
   slices: Slices
 }
 
 class Pizza {
-  constructor (flavor: string, slices: Slices) {
+  constructor (flavor: Comum | Doce | Vegetariana, slices: Slices) {
     this.flavor = flavor;
     this.slices = slices;
   }
 }
 
 const calabresa = new Pizza('calabresa', 8);
+const frango = new Pizza('frango', 4);
+const pepperoni = new Pizza('pepperoni', 4);
 const margherita = new Pizza('margherita', 6);
+const cogumelo = new Pizza('cogumelo', 4);
 const nutella = new Pizza('nutella', 4);

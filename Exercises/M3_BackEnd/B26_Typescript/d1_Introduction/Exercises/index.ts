4a65312e7b3f4b813b16ce924a99849e48352da6
1lucascq <1lucascq@gmail.com>
2022-03-24 13:32:02 -0300

    fix: corrige exercicio do dia 26-1

--- Exercises/M3_BackEnd/B26_Typescript/d1_Introduction/Exercises/index.ts
+++ Exercises/M3_BackEnd/B26_Typescript/d1_Introduction/Exercises/index.ts
@@ -4,8 +4,6 @@ import { lengthUnits } from './length';
 import massUnits from './mass';
 import exec from './utils';
 
-// FIXME: A importação executa as funções automaticamente, o que impede o código de funcionar
-
 const types: string[] =  ['length', 'mass', 'capacity'];
 const typesArr: string[][] = [lengthUnits, massUnits, capacityUnits]
 const selectedType: number = readline.keyInSelect(types, 'Escolha o tipo de conversão: ', { cancel: "SAIR" })

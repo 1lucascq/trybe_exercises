c3e85bf667e1479686a8e8dd7b044cc201d94206
1lucascq <1lucascq@gmail.com>
2022-03-24 12:49:32 -0300

    feat: exercicios de fixacao

---
+++ Exercises/M3_BackEnd/B26_Typescript/d2_Ecstatic_and_Generics_types/Class/fixation.ts
@@ -0,0 +1,80 @@
+type Bird = {
+  specie: 'with wings';
+  name: 'passarin'
+}
+
+type Sum = (
+  x: number,
+  y: number,
+) => number;
+
+type MaterType = 'solid' | 'liquid' | 'gas';
+
+type OS = 'linux' | 'windows';
+
+type CPF = '123.567.890-12' | '123456789012';
+
+enum EyeColor {
+  Black = "Pretos",
+  Blue = "Azuis",
+  Green = "Verdes",
+  Brown = "Castanhos",
+}
+
+// usamos a palavra reservada class para definir uma classe
+class Person {
+  name: string;
+  birthDate: Date; // o tipo Date está presente no TypeScript assim como no JavaScript
+  eyeColor: EyeColor; // na cor dos olhos usamos uma Enum com valores pré definidos
+
+  // aprenderemos mais sobre o construtor no próximo bloco
+  // considere-o como uma função utilizada para construir um objeto a partir da classe
+  // nele recebemos todos os dados necessários para construir um objeto de pessoa
+  constructor(name: string, birthDate: Date, eyeColor: EyeColor) {
+      // usamos o this para acessar as propriedades da instância da classe
+      // ele representa a própria instância que estamos criando
+      // atribuímos o valor do parâmetro recebido à propriedade da instância da classe
+      this.name  = name;
+      this.birthDate  = birthDate;
+      this.eyeColor  = eyeColor;
+  }
+
+  speak(): void {
+      console.log(`${this.name} está falando.`);
+  }
+
+  eat(): void {
+      console.log(`${this.name} está comendo.`)
+  }
+
+  walk(): void {
+      console.log(`${this.name} está andando.`)
+  }
+}
+
+// usamos a palavra reservada new para criar uma instância de Person
+// e passamos os parâmetros necessários para o construtor
+const person1 = new Person("Jane Doe", new Date("1986-01-01"), EyeColor.Brown);
+const person2 = new Person("Jon Doe", new Date("1980-08-05"), EyeColor.Black);
+
+console.log(person1);
+person1.speak()
+
+// saída:
+// Person: {
+//   "name": "Jane Doe",
+//   "birthDate": "1986-01-01T00:00:00.000Z",
+//   "eyeColor": "Castanhos"
+// }
+// "Jane Doe está falando."
+
+console.log(person2);
+person2.walk();
+
+// saída:
+// Person: {
+//   "name": "Jon Doe",
+//   "birthDate": "1980-08-05T00:00:00.000Z",
+//   "eyeColor": "Pretos"
+// }
+// "Jon Doe está andando."
